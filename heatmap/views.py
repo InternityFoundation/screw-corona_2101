85ba7ea8e34864909b0706d9eed1d0bd6ede2401
sanketp60 <spisat@hotmail.com>
2020-04-19 10:38:37 +0530

    Initial Code Run

--- heatmap/views.py
+++ heatmap/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'heatmap/index.html')
+
+    color_a = '#800080'
+    color_b = '#ffffff'
+    color_c = '#800080'
+    color_d = '#800080'
+    color_e = '#800080'
+    context={'color_a': color_a, 'color_b': color_b, 'color_c': color_c, 'color_d': color_d, 'color_e': color_e}
+    return render(request, 'heatmap/index.html', context)
