827e26167dd702e7dac06dac1c8b2836340a3b66
sanketp60 <spisat@hotmail.com>
2020-04-19 11:07:47 +0530

    First Working Prototype

--- heatmap/views.py
+++ heatmap/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render
 
+#dataset
+p = [210847,
+     140633,
+     202922,
+     382841,
+     440335
+     ]
+
+n1 = [5,
+      12,
+      9,
+      18,
+      5
+    ]
+
+n2 = [0,
+      0,
+      0,
+      3,
+      3
+    ]
+
+n3 = [1,
+      3,
+      3,
+      3,
+      2
+    ]
+
+def givecolor(n1,n2,n3,p):
+    val = ((n1*1.5+n2*2.6+n3*3.6)/p)*100
+    if val >=0.02:
+        return "#ff0000"
+    elif val>=0.01 and val<0.02:
+        return "#ffa500"
+    elif val>0 and val<0.01:
+        return "#0000ff"
 
 def index(request):
-
-    color_a = '#800080'
-    color_b = '#ffffff'
-    color_c = '#800080'
-    color_d = '#800080'
-    color_e = '#800080'
-    context={'color_a': color_a, 'color_b': color_b, 'color_c': color_c, 'color_d': color_d, 'color_e': color_e}
+    context = {"color_"+str(chr(x+65)):givecolor(n1[x], n2[x], n3[x], p[x]) for x in range(len(p))}
     return render(request, 'heatmap/index.html', context)
